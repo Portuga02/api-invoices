145823bc5e8c0f32b31f26aca1f45af8513bb80a
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2024-07-07 13:24:31 -0300

    ajustes

--- app/Http/Controllers/AutenticacaoController.php
+++ app/Http/Controllers/AutenticacaoController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AutenticacaoController extends Controller
 {
     public function login(Request $request)
     {
-        return $this->response('Authorized', 200);
+        return $this->response('Authorized', Response::HTTP_OK);
     }
 
     public function logout()
     {
+        return $this->response('Usuario deslogado com sucesso', Response::HTTP_OK);
     }
 
 }
