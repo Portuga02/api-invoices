4e0ba312662c7ad6df3de65d273f79628512be4b
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2024-07-08 13:28:02 -0300

    ajuste api

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,8 +12,8 @@ return new class () extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name',50);
-            $table->string('email',50);
+            $table->string('name',50)->nullable();
+            $table->string('email',50)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
